cfb8ce45bda1c24af28ade751b9738f1045b89b3
AndrewDyomin <dyomin.andrew1@gmail.com>
2023-08-13 00:22:50 +0300

    Initial

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from '../global-style';
+import { Feedback } from './feedback/feedback';
+
+export const App = () => {
+  return (
+    <>
+    <GlobalStyle />
+    <Feedback />
+    </>
+  );
+};
+
---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class Feedback extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div>
+                <h1>Please leave feedback</h1>
+            </div>
+        );
+    };
+};
